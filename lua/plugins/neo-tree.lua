3e6b9c6f8f40c75dfca0111084f3533b4c56fda5
邹昀昊 <zouyunhao@BananaComputer.modem>
2024-12-03 02:32:31 +1100

    add neo-tree's config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,5 +7,29 @@ return {
           "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
         },
+
+        config = function()
+            require("neo-tree").setup({
+                event_handlers = {
+                    -- Close Neotree after Open File
+                    {
+                        event = "file_open_requested",
+                        handler = function()
+                            -- auto close
+                            -- vim.cmd("Neotree close")
+                            -- OR
+                            -- require("neo-tree.command").execute({ action = "close" })
+                        end,
+                    },
+                },
+                window = {
+                    mappings = {
+                        ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+                        ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+                        ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+                    },
+                },
+            })
+        end,
     }
 }

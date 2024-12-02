856b663e4fdb09ec95938d77489c9555bf5b076e
邹昀昊 <zouyunhao@BananaComputer.modem>
2024-12-03 03:14:34 +1100

    Update noice's configuration

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,20 +8,11 @@ return {
         },
         config = function()
             require("noice").setup({
-                 presets = {
-                    bottom_search = true,
+                presets = {
+                    bottom_search = false,
                     long_message_to_split = true,
+                    command_palette = true,
                     lsp_doc_border = true,
-                },
-                cmdline_popup = {
-                    border = {
-                        style = "none",
-                        padding = { 2, 3 },
-                    },
-                    filter_options = {},
-                    win_options = {
-                        winhightlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-                    },
                 },
             })
         end

4c60bd61ca36bf4d0ac16f5a1aa5003d54dd5a53
邹昀昊 <zouyunhao@BananaComputer.modem>
2024-12-04 02:15:33 +1100

    add indent plugin

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufReadPre",
+        main = "ibl",
+        config = function()
+            vim.opt.list = true,
+
+            require("ibl").setup({
+                exclude = {
+                    filetypes = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
+                    buftypes = { "terminal", "nofile", "telescope" },
+                },
+                indent = {
+                    char = "│",
+                },
+                scope = {
+                    enabled = true,
+                    show_start = false,
+                },
+            })
+        end
+    }
+}

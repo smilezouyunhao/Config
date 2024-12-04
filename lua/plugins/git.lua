8868079faa8150939f281f65b11b64f611be8bcb
邹昀昊 <zouyunhao@BananaComputer.modem>
2024-12-04 21:45:39 +1100

    Normal update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -29,7 +29,7 @@ return {
         -- setting the keybinding for LazyGit with 'keys' is recommended in
         -- order to load the plugin when the command is run for the first time
         keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-        }
+            { "<leader>lg", ":LazyGit<cr>", desc = "LazyGit" },
+        },
     },
 }

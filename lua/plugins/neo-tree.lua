ef5e16c09fe1d4c5caef0987c787cf5ff179398c
邹昀昊 <zouyunhao@BananaComputer.modem>
2024-12-05 02:52:33 +1100

    Update neo-tree's configuration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,26 +7,45 @@ return {
           "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
         },
+        cmd = 'Neotree',
 
         config = function()
             require("neo-tree").setup({
+                popup_border_style = 'rounded',
+                opts = {
+                    close_if_last_window = true,
+                    sources = { 'filesystem', 'buffers', 'git_status' },
+                },
+                source_selector = {
+                    winbar = true,
+                    show_scrolled_off_parent_node = true,
+                    statusline = false,
+                    padding = { left = 1, right = 0 },
+                    sources = {
+                        { source = 'filesystem', display_name = '  Files' }, --      
+                        { source = 'buffers', display_name = '  Buffers' }, --      
+                        { source = 'git_status', display_name = ' 󰊢 Git' }, -- 󰊢      
+                    },
+                },
                 event_handlers = {
-                    -- Close Neotree after Open File
+                    -- Close neo-tree when opening a file.
                     {
-                        event = "file_open_requested",
+                        event = 'file_opened',
                         handler = function()
-                            -- auto close
-                            -- vim.cmd("Neotree close")
-                            -- OR
-                            -- require("neo-tree.command").execute({ action = "close" })
+                        require('neo-tree').close_all()
                         end,
                     },
                 },
                 window = {
                     mappings = {
-                        ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
-                        ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
-                        ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+                        ['<S-Tab>'] = 'prev_source',
+                        ['<Tab>'] = 'next_source',
+
+                        ['p'] = {
+                            'toggle_preview',
+                            nowait = true,
+                            config = { use_float = true },
+                        },
                     },
                 },
             })
